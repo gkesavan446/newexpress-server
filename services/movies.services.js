ae50b7842670fdbc5ec99acb6baeabb5102662df
gkesavan446 <gkesavan446@gmail.com>
2023-01-29 17:45:53 +0530

    objectid

--- services/movies.services.js
+++ services/movies.services.js
@@ -1,16 +1,17 @@
+import { ObjectId } from 'mongodb';
 import { client } from '../index.js';
 
 export async function updateMoviesById(id, data) {
-    return await client.db("newmongo").collection("newmovies").updateOne({ id: id }, { "$set": data });
+    return await client.db("newmongo").collection("newmovies").updateOne({ _id: ObjectId(id) }, { "$set": data });
 }
 export async function deleteMoviesById(id) {
-    return await client.db("newmongo").collection("newmovies").deleteOne({ id: id });
+    return await client.db("newmongo").collection("newmovies").deleteOne({ _id: ObjectId(id) });
 }
 export async function createMovies(data) {
-    return await client.db("newmongo").collection("newmovies").insertMany(data);
+    return await client.db("newmongo").collection("newmovies").insertOne(data);
 }
 export async function getMoviesById(id) {
-    return await client.db("newmongo").collection("newmovies").findOne({ id: id });
+    return await client.db("newmongo").collection("newmovies").findOne({ _id: ObjectId(id) });
 }
 export async function getAllMovies(request) {
     return await client.db("newmongo").collection("newmovies").find(request.query).toArray();
